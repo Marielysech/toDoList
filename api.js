296ba2c891ec18df3faa82a43ca9453dcc95047a
Marie-Lyse Charriere <marielysech@gmail.com>
2022-04-06 12:36:19 +0200

    setting up the server in api.js

---
+++ api.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+app.use(express.json())
+
+const port = 3000
+const hostName = 'localhost'
+
+app.listen(port, hostName, (err) => {
+    if (err) console.log("Something went wrong " + err );
+    else console.log(`Server running on port ${port}...`);
+})
+
+
+// '/tasks/' endpoint
+app.route('/tasks')
+    .get((req, res) => {
+        res.sendFile(__dirname + `/index.html`);
+        // res.send('here are all the tasks');
+    }) //access all tasks
+
+    .post((req, res) => {
+        res.send(`create the task ${req.params}`);
+    }); //create new task
+
+
+// '/tasks/:id' endpoint
+app.route('/tasks/:id')
+    .get((req, res) => {
+        res.send(`here is the ${req.params.id} task`);
+    }) //access specific tasks
+
+    .put((req, res) => {
+    res.send(`update the   ${req.params.id} task`);
+
+    }) //update specific tasks
+
+    .delete((req, res) => {
+    res.send(`delete the ${req.params.id} task`);
+    }); //supress specific tasks
+
+
+// /tasks/:id/:status
+app.put('/tasks/:id/:status', (req, res) => {
+    res.send(`modify your ${req.params.id} task status`);
+
+}); // modify status from specific task
+
+// /tasks/:id/:tag
+app.put('/tasks/:id/:tag', (req, res) => {
+    res.send('modify your task tag');
+
+}); //update tag of a specific task
+
+
+// /tasks/:status
+app.delete('/tasks/:status', (req, res) => {
+    res.send('modify your task status');
+}); // remove all tasks from specific status
